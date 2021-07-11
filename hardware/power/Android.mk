1250e7df54fe221c5592bd4b01a8f58c3a1a3169
ExpressLuke <luke@turbox.uk>
2021-07-11 19:32:19 +0100

    universal8890: Remove Lineage Power HAL

--- hardware/power/Android.mk
+++ hardware/power/Android.mk
@@ -32,8 +32,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhidlbase \
     libhidltransport \
     libutils \
-    android.hardware.power@1.0 \
-    vendor.lineage.power@1.0
+    android.hardware.power@1.0
 
 LOCAL_STATIC_LIBRARIES := libc++fs
 
